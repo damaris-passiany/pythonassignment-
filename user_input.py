347493db8d2016b3fb44b396872401975377f49b
damaris-passiany <soilapassiany@gmail.com>
2024-11-09 00:12:26 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# Asking for user input
+name = input("Damaris Passiany ")
+age = input("32 ")
+location = input("Narok ")
+
+# Displaying a personalized message
+print(f"Hello, {name}! You are {age} years old and from {location}.It's great to meet you!")
